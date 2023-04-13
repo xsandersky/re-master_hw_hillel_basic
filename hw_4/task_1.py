25163ff1b3a27dd3c314fb0c07ffa472181f39de
xsandersky <xsandersky@gmail.com>
2023-04-13 18:38:32 +0300

    Добавлено hw_4. В не добавлена работа task_1.py и __init__.py

---
+++ hw_4/task_1.py
@@ -0,0 +1,12 @@
+# Написати програму, що перетворює значення рядкової змінної з формату snake_case в формат CapitalizedWords
+# (a.k.a. Capitalized camelCase). Значення змінної отримуйте з input(). Для простоти вважаємо,
+# що значення змінної завжди складається з 3-х слів, але, можна оброблювати і "загальний випадок",
+# тобто, коли будь-яка кількість слів. Наприклад: 'employee_first_name' -> 'EmployeeFirstName'.
+text = input('Input some text: ')
+lst = text.split('_')
+
+lst = [i.capitalize() for i in lst]
+final_str = ''.join(lst)
+
+print(final_str)
+
