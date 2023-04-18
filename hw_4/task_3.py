6ca824be656a3b0ab7998bb66e0ece4b92e790da
xsandersky <xsandersky@gmail.com>
2023-04-18 12:05:57 +0300

    Добавлено hw_4. Добавил решение в task_3.py

---
+++ hw_4/task_3.py
@@ -0,0 +1,7 @@
+input_num = int(input('Please enter an integer number: '))
+
+output_txt = f'''Next number for number {input_num} is {input_num + 1}
+Previous number for number {input_num} is {input_num - 1}
+'''
+
+print(output_txt)
