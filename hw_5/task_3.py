f10e438bbef43e6d2ccaff023667c1b5a639eccf
xsandersky <xsandersky@gmail.com>
2023-04-18 21:52:39 +0300

    Добавлено hw_5. Добавил решение в task_3.py

---
+++ hw_5/task_3.py
@@ -0,0 +1,15 @@
+from math import pi
+def cone_square_and_volume(radius, height):  # returns 2 floats
+    sq = pi * radius**2 + pi * radius * (radius**2 + height**2)**0.5
+    vol = pi * radius**2 * height / 3
+
+    return sq, vol
+
+
+if __name__=="__main__":
+    get_radius = int(input('Input radius: '))
+    get_height = int(input('Input height: '))
+
+    square, volume = cone_square_and_volume(get_radius, get_height)
+
+    print(f'Square of cone = {square}\nVolume = {volume}')
