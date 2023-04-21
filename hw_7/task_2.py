3a91ece1e4e947191efa47f9fa9fa92c1e5baeee
xsandersky <xsandersky@gmail.com>
2023-04-21 20:24:15 +0300

    Добавлено hw_7. В task_2.py

---
+++ hw_7/task_2.py
@@ -0,0 +1,25 @@
+def my_func(start, end):
+    lst = []
+
+    for i in range(start, end + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            lst.append('FizzBuzz')
+        elif i % 5 == 0:
+            lst.append('Buzz')
+        elif i % 3 == 0:
+            lst.append('Fizz')
+        else:
+            lst.append(i)
+
+    return '\n'.join(map(str, lst))
+
+
+def main():
+    start = 1
+    end = 100
+
+    print(my_func(start, end))
+
+
+if __name__ == '__main__':
+    main()
