a2676375cddfc1a76b42aed0b75ee031cc7e0c6a
xsandersky <xsandersky@gmail.com>
2023-04-21 13:46:28 +0300

    Добавлено hw_6. В task_4.py сократил функцию

--- hw_6/task_4.py
+++ hw_6/task_4.py
@@ -1,10 +1,6 @@
 def circles_intersect(x1, y1, r1, x2, y2, r2): # returns boolean value
     distance_between_center = (x1 - x2)**2 + (y1 - y2)**2
-
-    if (r2 - r1)**2 <= distance_between_center <= (r1 + r2)**2:
-        return True
-    else:
-        return False
+    return True if (r2 - r1)**2 <= distance_between_center <= (r1 + r2)**2 else False
 
 
 def main():
