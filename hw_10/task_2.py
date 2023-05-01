a7a76fbc2d110337c9b7bf2ba4da1c6e54067fe8
xsandersky <xsandersky@gmail.com>
2023-05-01 13:14:08 +0300

    Add hw_10. Added task_2.py

---
+++ hw_10/task_2.py
@@ -0,0 +1,40 @@
+# Створити функцію "генератор паролів", що повертає згенеровані паролі сумісні з наступними вимогами.
+# Можна використовувати лише криптостійкі функції для генерації паролю.
+# Вимоги:
+# Пароль складається мінімум з 8 символів, потрібну довжину приймати параметром, але не менше 8
+# В паролі допускаються лише великі та маленькі латинські літери, цифри та символ підкреслення "_".
+# Використайте змінні з модулю string (UKR)
+# Пароль обов'язково має містити щонайменше одну велику літеру, одну маленьку літеру та одну цифру
+# Не повинно бути більше 2 однакових символів підряд
+
+import secrets
+import string
+
+
+def gen_pass(n):
+    data_for_pass = string.ascii_letters + string.digits + '_'
+    while n < 8:
+        n = int(input('Input digit > 7: '))
+
+    new_pas = {}
+
+    while True:
+        while len(new_pas)!= n:
+            random_char = secrets.choice(data_for_pass)
+            new_pas[random_char] = id(random_char)
+
+        if any(c.islower() for c in new_pas) and\
+                any(c.isupper() for c in new_pas) and\
+                any(c.isdigit() for c in new_pas):
+            break
+
+    return ''.join(list(new_pas))
+
+
+def main():
+    count_of_char = int(input('Input from amount characters would create password: '))
+    print(gen_pass(count_of_char))
+
+
+if __name__ == '__main__':
+    main()
---
+++ test2.py
@@ -0,0 +1,3 @@
+a = {'1':2, '2':3}
+c = [i for i in a]
+print(c)
